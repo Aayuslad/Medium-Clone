d6e65cb11682a4d4d6b75fc08d821d9af388452a
aayush <aayushlad058@gmail.com>
2024-04-16 23:57:29 +0530

    working

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,7 +5,8 @@ import axios from "axios";
 import { Toaster } from "react-hot-toast";
 // axios.defaults.baseURL = "https://backend.aayushlad058.workers.dev";
 // axios.defaults.baseURL = "http://127.0.0.1:8787";
-axios.defaults.baseURL = "http://localhost:8080";
+// axios.defaults.baseURL = "http://localhost:8080";
+axios.defaults.baseURL = "https://medium-clone-one-ruddy.vercel.app";
 axios.defaults.withCredentials = true;
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
@@ -18,7 +19,7 @@ root.render(
 					border: "1px solid black",
 					padding: "8px 15px",
 					color: "#333",
-					fontWeight: "600"
+					fontWeight: "600",
 				},
 				iconTheme: {
 					primary: "black",
